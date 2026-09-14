8c9b81a5beeab101b8bf44926a4a49c542cff119
fadymondy <11937812+fadymondy@users.noreply.github.com>
2026-09-14 12:22:39 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Laravilt\Widgets\Tests;
 
+use Laravilt\Widgets\WidgetsServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
@@ -16,7 +17,7 @@ class TestCase extends Orchestra
     protected function getPackageProviders($app): array
     {
         return [
-            \Laravilt\Widgets\WidgetsServiceProvider::class,
+            WidgetsServiceProvider::class,
         ];
     }
 
